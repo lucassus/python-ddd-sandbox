4b3db79fb69d810173dcf3c4fd235f9d29f26b03
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-08 20:20:00 +0100

    Use pass

--- todos/domain/abstract_repository.py
+++ todos/domain/abstract_repository.py
@@ -8,16 +8,16 @@ from todos.domain.models import Todo
 class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def get(self, id: int) -> Optional[Todo]:
-        ...
+        pass
 
     @abc.abstractmethod
     def list(self) -> List[Todo]:
-        ...
+        pass
 
     @abc.abstractmethod
     def update(self, todo: Todo, completed_at: Optional[date]) -> Todo:
-        ...
+        pass
 
     @abc.abstractmethod
     def create(self, name: str) -> Todo:
-        ...
+        pass

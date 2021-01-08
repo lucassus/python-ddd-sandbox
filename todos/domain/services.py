4e5f82be55fea8ebc8f8bced54db91b67c165058
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-08 19:36:35 +0100

    Create errors

--- todos/domain/services.py
+++ todos/domain/services.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 
 from todos.db.abstract_repository import AbstractRepository
+from todos.domain.errors import TodoNotFoundError
 from todos.domain.models import Todo
 
 
+# TODO: Refactor to regular methods
 class Service:
     def __init__(self, repository: AbstractRepository):
         self._repository = repository
 
     def complete(self, id: int) -> Todo:
         todo = self._repository.get(id)
-        assert todo
+        if not todo:
+            raise TodoNotFoundError
 
         if todo.is_completed:
             return todo
@@ -19,7 +22,9 @@ class Service:
 
     def incomplete(self, id: int) -> Todo:
         todo = self._repository.get(id)
-        assert todo
+
+        if not todo:
+            raise TodoNotFoundError
 
         if not todo.is_completed:
             return todo

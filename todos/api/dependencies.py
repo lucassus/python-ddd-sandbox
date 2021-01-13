f897fc1163b625c0f439d7b8aacb86da178e70f7
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-13 07:59:01 +0100

    Dryup the service wrapper

--- todos/api/dependencies.py
+++ todos/api/dependencies.py
@@ -1,3 +1,5 @@
+import functools
+
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -5,7 +7,7 @@ from todos.domain.models.todo import Todo
 from todos.interfaces.abstract_repository import AbstractRepository
 from todos.interfaces.db.repository import Repository
 from todos.interfaces.db.session import SessionLocal
-from todos.service_layer.services import complete_todo, create_todo, incomplete_todo
+from todos.service_layer import services
 
 
 def get_session():
@@ -25,26 +27,20 @@ def get_repository(session: Session = Depends(get_session)) -> AbstractRepositor
 
 class Service:
     def __init__(self, session: Session = Depends(get_session)):
-        self._session = session
-        self._repository = Repository(session=session)
-
-    def create_todo(self, name: str) -> Todo:
-        return create_todo(
-            name,
-            repository=self._repository,
-            session=self._session,
+        deps = dict(
+            session=session,
+            repository=Repository(session=session),
         )
 
-    def complete_todo(self, id: int) -> Todo:
-        return complete_todo(
-            id,
-            repository=self._repository,
-            session=self._session,
-        )
+        self._create_todo = functools.partial(services.create_todo, **deps)
+        self._complete_todo = functools.partial(services.complete_todo, **deps)
+        self._incomplete_todo = functools.partial(services.incomplete_todo, **deps)
 
-    def incomplete_todo(self, id: int) -> Todo:
-        return incomplete_todo(
-            id,
-            repository=self._repository,
-            session=self._session,
-        )
+    def create_todo(self, *, name: str) -> Todo:
+        return self._create_todo(name=name)
+
+    def complete_todo(self, id: str) -> Todo:
+        return self._complete_todo(id=id)
+
+    def incomplete_todo(self, id: str) -> Todo:
+        return self._incomplete_todo(id=id)

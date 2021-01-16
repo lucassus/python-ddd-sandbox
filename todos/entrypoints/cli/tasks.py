f2424f7bb8f74cfad7f68fc50b1bc1353abe04a7
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-16 15:53:49 +0100

    wip

--- todos/entrypoints/cli/tasks.py
+++ todos/entrypoints/cli/tasks.py
@@ -11,56 +11,51 @@ app = typer.Typer()
 start_mappers()
 uow = UnitOfWork()
 
-# TODO: Figure out how to dry uow context
-
 
 @app.command(help="Prints the list of all tasks")
 def list():
-    with uow:
-        tasks = uow.repository.list()
+    tasks = uow.repository.list()
 
-        typer.echo(
-            tabulate(
-                [[task.id, task.name, task.completed_at] for task in tasks],
-                headers=["Id", "Name", "Completed At"],
-            )
+    typer.echo(
+        tabulate(
+            [[task.id, task.name, task.completed_at] for task in tasks],
+            headers=["Id", "Name", "Completed At"],
         )
+    )
 
 
 @app.command(help="Creates a new task")
 def create(
     name: str = typer.Option(..., help="Task name", prompt="Enter new task name")
 ):
-    with uow:
-        create_task(name, uow=uow)
-        list()
+    create_task(name, uow=uow)
+    list()
 
 
 @app.command(help="Completes a task with the given ID")
 def complete(id: int = typer.Option(..., help="ID of task to complete")):
-    with uow:
-        task = uow.repository.get(id)
+    task = uow.repository.get(id)
 
-        if task is None:
-            typer.secho(f"Cannot find a task with ID={id}", fg=typer.colors.RED)
-            raise typer.Exit(code=1)
+    if task is None:
+        typer.secho(f"Cannot find a task with ID={id}", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
 
-        complete_task(task, uow=uow)
-        list()
+    complete_task(task, uow=uow)
+    list()
 
 
 @app.command(help="Undo a task with the given ID")
 def incomplete(id: int = typer.Option(..., help="ID of task to incomplete")):
-    with uow:
-        task = uow.repository.get(id)
+    task = uow.repository.get(id)
 
-        if task is None:
-            typer.secho(f"Cannot find a task with ID={id}", fg=typer.colors.RED)
-            raise typer.Exit(code=1)
+    if task is None:
+        typer.secho(f"Cannot find a task with ID={id}", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
 
-        incomplete_task(task, uow=uow)
-        list()
+    incomplete_task(task, uow=uow)
+    list()
 
 
 if __name__ == "__main__":
-    app()
+    with uow:
+        app()

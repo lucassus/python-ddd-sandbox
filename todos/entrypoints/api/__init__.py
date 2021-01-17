0149bf4f29e1c0e5313bfc6b8de0d60710e7772d
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-17 16:00:26 +0100

    Reorganize api routes

--- todos/entrypoints/api/__init__.py
+++ todos/entrypoints/api/__init__.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
-# TODO: Rename endpoints to routes
-from todos.entrypoints.api.endpoints import health, projects, tasks
+from todos.entrypoints.api.routes import health, project_tasks, projects
 
 api_router = APIRouter()
 api_router.include_router(health.router)
 api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
 api_router.include_router(
-    tasks.router, prefix="/projects/{project_id}/tasks", tags=["tasks"]
+    project_tasks.router, prefix="/projects/{project_id}/tasks", tags=["tasks"]
 )

2a52be41bc8b76f6b2cdd97fa780eb8d634892fd
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-18 21:01:55 +0100

    Resolve project by id

--- todos/entrypoints/graphql/schema.py
+++ todos/entrypoints/graphql/schema.py
@@ -1,4 +1,4 @@
-from graphene import ID, Boolean, List, NonNull, ObjectType, String
+from graphene import ID, Boolean, Field, List, NonNull, ObjectType, String
 
 from todos.interfaces.abstract_unit_of_work import AbstractUnitOfWork
 
@@ -7,17 +7,23 @@ class Task(ObjectType):
     id = ID()
     name = NonNull(String)
     is_completed = NonNull(Boolean)
+    completed_at = String()
 
 
 class Project(ObjectType):
     id = ID()
     name = NonNull(String)
-    tasks = NonNull(List(Task))
+    tasks = NonNull(List(NonNull(Task)))
 
 
 class Query(ObjectType):
     projects = NonNull(List(NonNull(Project)))
+    project = Field(NonNull(Project), id=ID())
 
     def resolve_projects(self, info):
         uow: AbstractUnitOfWork = info.context["request"].state.uow
         return uow.repository.list()
+
+    def resolve_project(self, info, id):
+        uow: AbstractUnitOfWork = info.context["request"].state.uow
+        return uow.repository.get(id)

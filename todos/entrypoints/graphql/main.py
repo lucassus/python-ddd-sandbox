b6078c79bbf85ba02f71ebf6402accf4f66da750
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-18 18:20:05 +0100

    wip

--- todos/entrypoints/graphql/main.py
+++ todos/entrypoints/graphql/main.py
@@ -1,14 +1,37 @@
 import graphene
 from fastapi import FastAPI
+from graphene import ObjectType
 from starlette.graphql import GraphQLApp
+
+from todos.interfaces.db.tables import start_mappers
+from todos.interfaces.db.unit_of_work import UnitOfWork
+
+start_mappers()
+
+
+class Task(ObjectType):
+    id = graphene.ID()
+    name = graphene.NonNull(graphene.String)
+
+
+class Project(ObjectType):
+    id = graphene.ID()
+    name = graphene.NonNull(graphene.String)
+    tasks = graphene.NonNull(graphene.List(Task))
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
+    projects = graphene.NonNull(graphene.List(graphene.NonNull(Project)))
 
     def resolve_hello(self, info, name):
         return "Hello " + name
 
+    def resolve_projects(self, info):
+        with UnitOfWork() as uof:
+            return uof.repository.list()
+
 
 app = FastAPI()
-app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
+graphql_app = GraphQLApp(schema=graphene.Schema(query=Query))
+app.add_route("/", graphql_app)

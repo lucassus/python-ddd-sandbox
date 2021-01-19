d7f1e85ba8e7fecea3b06d7427da84ad4fdd4178
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-19 19:03:36 +0100

    wip

--- examples/inversion_of_control_3/inner/service_test.py
+++ examples/inversion_of_control_3/inner/service_test.py
@@ -3,11 +3,16 @@ from examples.inversion_of_control_3.inner.service import Service
 from examples.user import User
 
 
-class FakeRepository(AbstractRepository):
-    def get_user(self) -> User:
-        return User(name="Faker")
-
-
 def test_service():
+    # Given
+    class FakeRepository(AbstractRepository):
+        def get_user(self) -> User:
+            return User(name="Faker")
+
     service = Service(repository=FakeRepository())
-    assert service() == "Hello, Faker!"
+
+    # When
+    message = service()
+
+    # Then
+    assert message == "Hello, Faker!"

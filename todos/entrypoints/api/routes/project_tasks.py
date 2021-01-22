7c66310f2dbbd202abd3aeb34667b7a21d5d134f
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-22 21:29:00 +0100

    Initial spike for simple CQRS

--- todos/entrypoints/api/routes/project_tasks.py
+++ todos/entrypoints/api/routes/project_tasks.py
@@ -1,7 +1,8 @@
 from datetime import date
 from typing import List
 
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Path, status
+from fastapi.responses import RedirectResponse
 
 from todos.domain.entities import Project
 from todos.domain.service import Service
@@ -22,10 +23,18 @@ def tasks_endpoint(project: Project = Depends(get_project)):
 
 @router.post("", response_model=schemas.Task)
 def task_create_endpoint(
+    project_id: int,
     data: schemas.CreateTask,
     service: Service = Depends(get_service),
 ):
-    return service.create_task(data.name)
+    task = service.create_task(data.name)
+
+    # TODO: Almost works, find a way to redirect from POST to GET
+    # TODO:
+    return RedirectResponse(
+        url=f"/projects/{project_id}/tasks/{task.id}",
+        status_code=status.HTTP_303_SEE_OTHER,
+    )
 
 
 @router.get("/{id}", response_model=schemas.Task)

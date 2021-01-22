eef32938fc2671369d0532ad21fd24056aaebd26
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-22 09:30:26 +0100

    Simple decorator usage

--- examples/message_bus/message_bus.py
+++ examples/message_bus/message_bus.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Any, Callable, Dict, List
+from typing import Callable, Dict, List
 
 from examples.message_bus.event import Event
 
@@ -8,12 +8,11 @@ class MessageBus:
     def __init__(self):
         self._listeners: Dict[type, List[Callable]] = defaultdict(list)
 
-    # TODO: How to type a class?
-    def listen(self, event_class, handler: Callable[[Any, Event], None]):
-        if event_class not in self._listeners:
-            self._listeners[event_class] = []
+    def listen(self, event_class: type, handler=None):
+        def on(handler):
+            self._listeners[event_class].append(handler)
 
-        self._listeners[event_class].append(handler)
+        return on(handler) if handler else on
 
     def dispatch(self, event: Event) -> None:
         handlers = self._listeners.get(type(event), [])
--- examples/message_bus/message_bus_test.py
+++ examples/message_bus/message_bus_test.py
@@ -9,42 +9,56 @@ class SomeEvent(Event):
     message: str
 
 
-def test_message_bus():
-    bus = MessageBus()
+class TestMessageBus:
+    def test_simple_listener(self):
+        bus = MessageBus()
 
-    results = []
+        # TODO: Find a more pythonic way
+        results = []
 
-    def handle_some_event(event: SomeEvent):
-        results.append(event.message)
+        def handle_some_event(event: SomeEvent):
+            results.append(event.message)
 
-    bus.listen(SomeEvent, handle_some_event)
+        bus.listen(SomeEvent, handle_some_event)
 
-    bus.dispatch(SomeEvent(message="Hello!"))
+        bus.dispatch(SomeEvent(message="Hello!"))
 
-    assert len(results) == 1
-    assert results[-1] == "Hello!"
+        assert len(results) == 1
+        assert results[-1] == "Hello!"
 
+    def test_decorator_listener(self):
+        bus = MessageBus()
 
-def test_message_bus_event_can_have_multiple_handlers():
-    bus = MessageBus()
+        results = []
 
-    results = []
+        @bus.listen(SomeEvent)
+        def handle_some_event(event: SomeEvent):
+            results.append(event.message)
 
-    def handle_event_1(event: SomeEvent):
-        results.append(event.message)
+        bus.dispatch(SomeEvent(message="Hello!"))
 
-    bus.listen(SomeEvent, handle_event_1)
+        assert len(results) == 1
+        assert results[-1] == "Hello!"
 
-    def handle_event_2(event: SomeEvent):
-        results.append(event.message)
+    def test_event_can_have_multiple_handlers(self):
+        bus = MessageBus()
 
-    bus.listen(SomeEvent, handle_event_2)
+        results = []
 
-    bus.dispatch(SomeEvent(message="Hello!"))
+        def handle_event_1(event: SomeEvent):
+            results.append(event.message)
 
-    assert len(results) == 2
+        bus.listen(SomeEvent, handle_event_1)
 
+        def handle_event_2(event: SomeEvent):
+            results.append(event.message)
 
-def test_message_bus_does_nothing_when_the_message_cannot_be_handled():
-    bus = MessageBus()
-    bus.dispatch(SomeEvent(message="Hello!"))
+        bus.listen(SomeEvent, handle_event_2)
+
+        bus.dispatch(SomeEvent(message="Hello!"))
+
+        assert len(results) == 2
+
+    def test_do_nothing_when_the_message_cannot_be_handled(self):
+        bus = MessageBus()
+        bus.dispatch(SomeEvent(message="Hello!"))

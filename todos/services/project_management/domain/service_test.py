a2d8dec969016cafb25b82e132b5aa8c71ab99f2
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-24 18:27:07 +0100

    More elegant test with proper fixtures

--- todos/services/project_management/domain/service_test.py
+++ todos/services/project_management/domain/service_test.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+import pytest
+
 from todos.services.project_management.domain.service import Service
 from todos.services.project_management.test_utils.factories import (
     build_project,
@@ -9,21 +11,34 @@ from todos.services.project_management.test_utils.fake_unit_of_work import (
     FakeUnitOfWork,
 )
 
-project = build_project(id=1)
-task = build_task(id=2)
-project.tasks = [task]
-fake_uow = FakeUnitOfWork([project])
-service = Service(uow=fake_uow)
+
+@pytest.fixture
+def project():
+    project = build_project(id=1)
+    project.tasks = [build_task(id=2)]
+
+    return project
 
 
-def test_create_task():
+@pytest.fixture
+def fake_uow(project):
+    return FakeUnitOfWork([project])
+
+
+@pytest.fixture
+def service(fake_uow):
+    return Service(uow=fake_uow)
+
+
+def test_create_task(service, project, fake_uow):
     service.create_task(project_id=project.id, name="Testing...")
 
     assert project.tasks[-1].name == "Testing..."
     assert fake_uow.committed
 
 
-def test_complete_task():
+def test_complete_task(service, project, fake_uow):
+    task = project.tasks[-1]
     now = date(2021, 1, 8)
     service.complete_task(project_id=project.id, id=task.id, now=now)
 
@@ -31,7 +46,8 @@ def test_complete_task():
     assert fake_uow.committed
 
 
-def test_incomplete_task():
+def test_incomplete_task(service, project, fake_uow):
+    task = project.tasks[-1]
     service.incomplete_task(project_id=project.id, id=task.id)
 
     assert task.completed_at is None

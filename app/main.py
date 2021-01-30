3b2af792266b2d461aa9151208968b625cbc5bd3
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-30 15:10:53 +0100

    Refactor bootstrap script

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
+from fastapi import FastAPI
+
 from app.infrastructure.session import engine
 from app.infrastructure.tables import create_tables
 from app.modules.app import create_app as create_commands_app
-from app.query.app import create_app
+from app.query.app import create_app as create_queries_app
 
 create_tables(engine)
 
-app = create_app()
+app = FastAPI()
 
-commands_app = create_commands_app()
-app.mount("/commands", commands_app)
+app.mount("/", create_queries_app())
+app.mount("/commands", create_commands_app())

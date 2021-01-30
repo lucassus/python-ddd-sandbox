d81cb34b9476b140864a94f42cd9bebbe9b181e1
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-30 12:46:29 +0100

    Improve bootstrap script

---
+++ app/modules/app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+
+from app.common.errors import EntityNotFoundError
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    from app.modules.accounts import api_router, start_mappers
+
+    start_mappers()
+    app.include_router(api_router)
+
+    from app.modules.projects import api_router, start_mappers
+
+    start_mappers()
+    app.include_router(api_router)
+
+    @app.exception_handler(EntityNotFoundError)
+    async def unicorn_exception_handler(request: Request, exc: EntityNotFoundError):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": exc.message},
+        )
+
+    return app

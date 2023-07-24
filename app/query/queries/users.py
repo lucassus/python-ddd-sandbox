62aaf60e00e69a8d64ac65ef4d425c74dcf9ed06
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-07-24 11:25:26 +0200

    Cleanup

--- app/query/queries/users.py
+++ app/query/queries/users.py
@@ -1,17 +1,8 @@
-from sqlalchemy import select
-
 from app.infrastructure.tables import projects_table, users_table
 from app.query.queries.abstract_query import AbstractQuery
 
 
-# TODO: Join projects
 class FindUserQuery(AbstractQuery):
     def __call__(self, *, id: int):
         query = users_table.outerjoin(projects_table).select().where(users_table.c.id == id)
-        # query = (
-        #     select([users_table])
-        #     .where(users_table.c.id == id)
-        #     .outerjoin(projects_table)
-        # )
-        print(query)
         return self._database.fetch_one(query)

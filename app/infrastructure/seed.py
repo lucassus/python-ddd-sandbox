22892020d03f00d41738d5616ca4abdfc2f84d9d
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-07-25 06:51:56 +0200

    Make the seed transactional

--- app/infrastructure/seed.py
+++ app/infrastructure/seed.py
@@ -13,30 +13,31 @@ def main(rebuild_db: bool = True):
         drop_tables(engine)
         create_tables(engine)
 
-    connection = engine.connect()
+    with engine.connect() as connection:
+        user_id = create_user(connection, email="test@email.com").id
+        project_id = create_project(connection, user_id, name="Project One").id
+        create_project(connection)
 
-    create_project(connection)
-    user_id = create_user(connection, email="test@email.com").id
-    project_id = create_project(connection, user_id, name="Project One").id
+        create_task(connection, project_id, name="Learn Python", completed_at=date(2021, 1, 6))
+        create_task(connection, project_id, name="Learn Domain Driven Design")
+        create_task(connection, project_id, name="Do the shopping")
+        create_task(connection, project_id, name="Clean the house")
 
-    create_task(connection, project_id, name="Learn Python", completed_at=date(2021, 1, 6))
-    create_task(connection, project_id, name="Learn Domain Driven Design")
-    create_task(connection, project_id, name="Do the shopping")
-    create_task(connection, project_id, name="Clean the house")
-
-    tasks = connection.execute(tasks_table.select())
+        tasks = connection.execute(tasks_table.select()).all()
+        connection.commit()
 
     typer.echo(
         tabulate(
             [
                 [
                     task.id,
+                    task.project_id,
                     task.name,
                     task.completed_at,
                 ]
                 for task in tasks
             ],
-            headers=["Id", "Name", "Completed At"],
+            headers=["Id", "ProjectId", "Name", "Completed At"],
         ),
     )
     typer.echo("\nSeeding completed 🚀")

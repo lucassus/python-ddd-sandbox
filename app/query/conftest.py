73acaa34e0eb7cefed134ee283a3695425b46d3e
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-01 08:35:02 +0200

    Throw away e2e test for queries

--- app/query/conftest.py
+++ app/query/conftest.py
@@ -2,15 +2,13 @@ import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
 
-from app.query.queries.abstract_query import get_connection
 from app.query.routes import api_router
 
 
 @pytest.fixture
-def app(connection):
+def app():
     app = FastAPI()
     app.include_router(api_router)
-    app.dependency_overrides[get_connection] = lambda: connection
 
     return app
 

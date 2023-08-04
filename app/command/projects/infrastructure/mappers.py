6fa0649a18f60f480a332493c938666f7a720665
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-04 08:45:38 +0200

    wip

--- app/command/projects/infrastructure/mappers.py
+++ app/command/projects/infrastructure/mappers.py
@@ -14,7 +14,10 @@ def start_mappers(mapper_registry):
             "_name": projects_table.c.name,
             "_maximum_number_of_incomplete_tasks": projects_table.c.maximum_number_of_incomplete_tasks,
             "_last_task_number": projects_table.c.last_task_number,
-            "_tasks_by_number": relationship(Task, collection_class=attribute_keyed_dict("number")),
+            "_tasks_by_number": relationship(
+                Task,
+                collection_class=attribute_keyed_dict("number"),
+            ),
             "_archived_at": projects_table.c.archived_at,
             "_deleted_at": projects_table.c.deleted_at,
         },

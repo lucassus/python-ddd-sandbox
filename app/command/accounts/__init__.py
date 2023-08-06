7b6930298de03ccd8a27ce23e0698e9f3940803f
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-06 18:24:58 +0200

    WIP

--- app/command/accounts/__init__.py
+++ app/command/accounts/__init__.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import registry
 
 
 def register_module(app: FastAPI, mappers: registry) -> None:
-    from app.command.accounts.entrypoints.endpoints import router
+    from app.command.accounts.entrypoints import endpoints
+    from app.command.accounts.entrypoints.containers import Container
     from app.command.accounts.infrastructure.mappers import start_mappers
 
+    container = Container()
+    container.wire(modules=[endpoints])
+
     start_mappers(mappers)
-    app.include_router(router)
+    app.include_router(endpoints.router)
--- app/command/accounts/entrypoints/containers.py
+++ app/command/accounts/entrypoints/containers.py
@@ -3,11 +3,12 @@ from dependency_injector import containers, providers
 from app.command.accounts.application.change_user_email_address import ChangeUserEmailAddress
 from app.command.accounts.application.register_user import RegisterUser
 from app.command.accounts.infrastructure.adapters.unit_of_work import UnitOfWork
+from app.command.event_handlers import bus  # TODO: This should not import from the parent module
 from app.infrastructure.db import AppSession
 
 
 class Container(containers.DeclarativeContainer):
     uow = providers.Factory(UnitOfWork, session_factory=lambda: AppSession())
 
-    register_user = providers.Factory(RegisterUser, uow=uow)
+    register_user = providers.Factory(RegisterUser, uow=uow, bus=bus)
     change_user_email_address = providers.Factory(ChangeUserEmailAddress, uow=uow)

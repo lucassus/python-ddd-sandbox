1e2f6d7426690288ac1ee0ee618a0cb8e2d282c1
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-06 19:24:42 +0200

    Remove noop message bus

--- app/command/accounts/application/register_user.py
+++ app/command/accounts/application/register_user.py
@@ -1,28 +1,16 @@
-from typing import Optional
-
 from app.command.accounts.application.ports.abstract_unit_of_work import AbstractUnitOfWork
 from app.command.accounts.entities.email_address import EmailAddress
 from app.command.accounts.entities.errors import EmailAlreadyExistsException
 from app.command.accounts.entities.password import Password
 from app.command.accounts.entities.user import User
 from app.command.shared_kernel.entities.user_id import UserID
-from app.command.shared_kernel.message_bus import BaseEvent, MessageBus
-
-
-class NoopMessageBus(MessageBus):
-    def dispatch(self, event: BaseEvent) -> None:
-        pass
+from app.command.shared_kernel.message_bus import MessageBus
 
 
 class RegisterUser:
-    def __init__(
-        self,
-        *,
-        uow: AbstractUnitOfWork,
-        bus: Optional[MessageBus] = None,
-    ):
+    def __init__(self, *, uow: AbstractUnitOfWork, bus: MessageBus):
         self._uow = uow
-        self._bus = bus if bus is not None else NoopMessageBus()
+        self._bus = bus
 
     def __call__(self, *, email: EmailAddress, password: Password) -> UserID:
         with self._uow as uow:

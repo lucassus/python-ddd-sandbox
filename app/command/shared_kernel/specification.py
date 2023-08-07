58638ee75489029aabdd224b725198f01fc1ea21
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-07 17:12:43 +0200

    Add specification pattern

---
+++ app/command/shared_kernel/specification.py
@@ -0,0 +1,59 @@
+import abc
+from abc import ABCMeta
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class Specification(Generic[T], metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def is_satisfied_by(self, candidate: T) -> bool:
+        raise NotImplementedError
+
+    def __call__(self, candidate: T) -> bool:
+        return self.is_satisfied_by(candidate)
+
+    def __and__(self, other: "Specification[T]") -> "Specification[T]":
+        return And(self, other)
+
+    def __or__(self, other: "Specification[T]") -> "Specification[T]":
+        return Or(self, other)
+
+    def __invert__(self) -> "Specification[T]":
+        return NotSpecification(self)
+
+
+class BinarySpecification(Specification[T], metaclass=ABCMeta):
+    def __init__(self, left: Specification[T], right: Specification[T]):
+        self._left = left
+        self._right = right
+
+
+class And(BinarySpecification[T]):
+    def is_satisfied_by(self, candidate: T) -> bool:
+        return self._left.is_satisfied_by(candidate) and self._right.is_satisfied_by(candidate)
+
+
+class Or(BinarySpecification[T]):
+    def is_satisfied_by(self, candidate: T) -> bool:
+        return self._left.is_satisfied_by(candidate) or self._right.is_satisfied_by(candidate)
+
+
+class UnarySpecification(Specification[T], metaclass=ABCMeta):
+    def __init__(self, spec: Specification[T]):
+        self._spec = spec
+
+
+class NotSpecification(UnarySpecification[T]):
+    def is_satisfied_by(self, candidate: T) -> bool:
+        return not self._spec.is_satisfied_by(candidate)
+
+
+class AlwaysTrue(Specification[T]):
+    def is_satisfied_by(self, candidate: T) -> bool:
+        return True
+
+
+class AlwaysFalse(Specification[T]):
+    def is_satisfied_by(self, candidate: T) -> bool:
+        return False
---
+++ app/command/shared_kernel/specification_test.py
@@ -0,0 +1,57 @@
+from py_katas.specification.specification import AlwaysFalse, AlwaysTrue, Specification
+
+
+class GreaterThan(Specification[int]):
+    def __init__(self, number: int):
+        self._number = number
+
+    def is_satisfied_by(self, candidate):
+        return candidate > self._number
+
+
+class SmallerThan(Specification[int]):
+    def __init__(self, number: int):
+        self._number = number
+
+    def is_satisfied_by(self, candidate):
+        return candidate < self._number
+
+
+class IsEven(Specification[int]):
+    def is_satisfied_by(self, candidate):
+        return candidate % 2 == 0
+
+
+def test_specification():
+    spec = GreaterThan(7)
+
+    assert spec.is_satisfied_by(8) is True
+    assert spec.is_satisfied_by(7) is False
+
+
+def test_and_specification():
+    spec = GreaterThan(7) & IsEven() & SmallerThan(10)
+
+    assert spec.is_satisfied_by(8) is True
+    assert spec.is_satisfied_by(6) is False
+    assert spec.is_satisfied_by(9) is False
+    assert spec.is_satisfied_by(10) is False
+
+
+def test_or_specification():
+    spec = GreaterThan(3) | IsEven()
+
+    assert spec.is_satisfied_by(2) is True
+    assert spec.is_satisfied_by(5) is True
+
+
+def test_always_true_specification():
+    spec = AlwaysTrue[int]()
+
+    assert spec.is_satisfied_by(2) is True
+
+
+def test_always_false_specification():
+    spec = AlwaysFalse[int]()
+
+    assert spec.is_satisfied_by(2) is False

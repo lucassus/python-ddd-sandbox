579723e7bb04d3bd3716ae22c905475d0348a422
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-08 15:02:04 +0200

    Add token expiration time

--- app/command/accounts/application/authenticate.py
+++ app/command/accounts/application/authenticate.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timedelta
+
 import jwt
 
 from app.command.accounts.application.ports.abstract_unit_of_work import AbstractUnitOfWork
 from app.command.accounts.entities.email_address import EmailAddress
 from app.command.accounts.entities.password import Password
+from app.utc_datetime import utc_now
 
 
 class AuthenticationError(Exception):
@@ -14,7 +17,15 @@ class Authenticate:
         self._uow = uow
         self._secret_auth_key = secret_auth_key
 
-    def __call__(self, email: EmailAddress, password: Password) -> str:
+    def __call__(
+        self,
+        email: EmailAddress,
+        password: Password,
+        now: datetime | None = None,
+    ) -> str:
+        if now is None:
+            now = utc_now()
+
         with self._uow as uow:
             user = uow.user.get_by_email(email)
 
@@ -25,7 +36,11 @@ class Authenticate:
                 raise AuthenticationError()
 
             return jwt.encode(
-                payload={"sub": str(user.id)},
+                payload={
+                    "sub": str(user.id),
+                    "exp": now + timedelta(days=90),
+                    "iat": now,
+                },
                 key=self._secret_auth_key,
                 algorithm="HS256",
             )

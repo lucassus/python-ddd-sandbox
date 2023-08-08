dfd5539593587e4a7431ff64de0a28d7d52cd5a4
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2023-08-08 09:05:02 +0200

    Dry up the schema

--- app/command/accounts/entrypoints/schemas.py
+++ app/command/accounts/entrypoints/schemas.py
@@ -2,11 +2,13 @@ from pydantic import Field
 
 from app.base_schema import BaseSchema
 
+EmailField = Field(..., title="User email address", min_length=4, max_length=32)
+
 
 class RegisterUser(BaseSchema):
-    email: str = Field(..., title="User email address", min_length=4, max_length=32)
+    email: str = EmailField
     password: str = Field(..., min_length=6)
 
 
 class UpdateUser(BaseSchema):
-    email: str = Field(..., title="User email address", min_length=4, max_length=32)
+    email: str = EmailField

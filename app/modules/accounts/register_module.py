96583a5153eb18a242d6aacd990a4348965f2b65
Lukasz Bandzarewicz <lukasz.bandzarewicz@maxio.com>
2023-08-18 16:51:53 +0200

    Add task.created_by field

--- app/modules/accounts/register_module.py
+++ app/modules/accounts/register_module.py
@@ -2,7 +2,8 @@ from dependency_injector import providers
 from fastapi import FastAPI
 from sqlalchemy.orm import registry
 
-from app import engine
+from app.config import settings
+from app.infrastructure.db import engine
 from app.modules.accounts.entrypoints import routes
 from app.modules.accounts.entrypoints.containers import Container
 from app.modules.accounts.infrastructure.mappers import start_mappers
@@ -13,7 +14,7 @@ from app.modules.shared_kernel.message_bus import MessageBus
 def register_module(app: FastAPI, mappers: registry, bus: MessageBus) -> AuthenticationContract:
     container = Container(
         engine=engine,
-        jwt_secret_key="some secret",  # TODO: Take it from the proper config
+        jwt_secret_key=settings.jwt_secret_key,
         bus=providers.Object(bus),
     )
     container.wire()
